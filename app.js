99ca9ac730768b1120d2fcc15e96e303aadd057c
Dev-PeterWachira <wmainap@gmail.com>
2024-10-21 08:39:23 +0300

    db.json file

---
+++ app.js
@@ -0,0 +1,12 @@
+{
+    "books": [
+      { "id": 1, "title": "Book One", "author": "Author One", "review": "Great read!" },
+      { "id": 2, "title": "Book Two", "author": "Author Two", "review": "Informative!" }
+    ]
+  }
+  
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+const apiURL = 'https://exampleapi.com/books';
